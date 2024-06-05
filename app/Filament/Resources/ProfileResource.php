9a46389bf3365a9a11d7e6480f2e80c947adacb6
unknown <tonyentertain@gmail.com>
2024-06-05 01:03:03 +0100

    Added Extra Features to the User Management

--- app/Filament/Resources/ProfileResource.php
+++ app/Filament/Resources/ProfileResource.php
@@ -34,25 +34,38 @@ class ProfileResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\TextColumn::make('user.ref_num')
+                    ->searchable()
+                    ->label('Student Ref#'),
                 Tables\Columns\TextColumn::make('user.surname')
+                    ->searchable()
                     ->label('surname'),
                 Tables\Columns\TextColumn::make('user.first_name')
+                    ->searchable()
                     ->label('Other Names')
                     ->formatStateUsing(function (Profile $profile) {
                         return $profile->user->first_name . ' ' . $profile->user->middle_name;
                     }),
                 Tables\Columns\TextColumn::make('dept')
+                    ->searchable()
                     ->label('Dept'),
                 Tables\Columns\TextColumn::make('entry_year'),
                 Tables\Columns\TextColumn::make('approval_date'),
                 Tables\Columns\TextColumn::make('completed')
+                    
                     ->formatStateUsing(function (Profile $profile) {
-                        if($profile->profile === true)
+                        if($profile->completed == true)
                         {return 'Yes';} return 'No';
                     }),
             ])
             ->filters([
-                //
+                Tables\Filters\Filter::make('completed')
+                    ->toggle()
+                    ->query(fn (Builder $query): Builder => $query->where('completed', 1)),
+                Tables\Filters\Filter::make('passed_final_exam')
+                    ->toggle()
+                    ->label('Passed Exams')
+                    ->query(fn (Builder $query): Builder => $query->where('passed_final_exam', 0))
             ])
             ->actions([
                 Tables\Actions\ViewAction::make(),
